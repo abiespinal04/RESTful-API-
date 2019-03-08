6bd8fd4e7d3ddb5640215098dcfbe4814b811b0a
Abimael Espinal <abimaelespinal@hotmail.com>
2019-03-08 02:49:08 -0500

    Connected the app to a mongodb cluster and made the products route able get and post data into the cluster/db

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const Product = require('../models/product');
+const mongoose = require('mongoose');
+
 router.get('/', (req, res,next) => {
     res.status(200).json({
         message: 'Handling Get request to /products'
@@ -13,7 +16,17 @@ router.post('/', (req,res,next) =>{
     const product = {
         name: req.body.name,
         price: req.body.price
-    }
+    };
+
+    //moongose product
+     const mproduct = new Product({
+            _id: new mongoose.Types.ObjectId(),
+            name: req.body.name,
+            price: req.body.price
+     });
+     mproduct.save().then(result => {
+         console.log(result);
+     }).catch(err => console.log(err));
 
     res.status(201).json({
         message: 'Handling POST request to /products',
@@ -24,19 +37,17 @@ router.post('/', (req,res,next) =>{
 });
 
 router.get('/:productId', (req,res,next) => {
+   
     const id = req.params.productId;
-
-    if(id === 'special'){
-        res.status(200).json({
-            message:'You discovered the special ID',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
-
+    Product.findById(id)
+    .exec()
+    .then(doc =>{
+        console.log(doc);
+        res.status(200).json(doc);
+    })
+    .catch(err => {console.log(err);
+        res.status(500).json({error:err});
+    });
 });
 
 router.patch('/:productId', (req,res,next) => {
--- app.js
+++ app.js
@@ -2,16 +2,28 @@ const express = require('express');
 app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//connection to mongodb
+mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@rest-node-shop-3szv9.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true });
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
+//CORS Handling
 app.use((req,res,next) => {
+    /*
+    //This header allows access to only the specified client
+    // res.header('Access-Control-Allow-Origin', 'http:/my-cool-page.com');
+    */
+    
+    //This header allows access to every client domain
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
@@ -26,6 +38,7 @@ app.use((req,res,next) => {
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+//Handling errors for invalid routes
 app.use((req,res,next) => {
     const error = new Error('Not Found');
     error.status = 404;
